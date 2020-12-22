a9d22defaa151c09413365256726ac84229692e6
tobao17 <phamtobao99@gmail.com>
2020-12-23 02:39:42 +0700

    push heroku2

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.ico"));
+
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
+app.use(favicon(__dirname + "/build/favicon.ico"));
 app.get("/ping", function (req, res) {
 	return res.send("pong");
 });
